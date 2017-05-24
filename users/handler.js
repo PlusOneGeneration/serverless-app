fc2588bb14b9c7d56d76c0b287871266ef6f7d2a
slava hatnuke <slava.hatnuke@gmail.com>
2017-05-25 01:56:49 +0300

    init

---
+++ users/handler.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.create = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'OK',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
